f97ad4f68cb87aff8f6a91606cebbce26f3d530c
Edwin G Encarnacion unknown student <ege2@afsconnect1.njit.edu>
2015-12-09 19:46:37 -0500

    fixing outlook

--- src/contact-form-proc.php
+++ src/contact-form-proc.php
@@ -3,37 +3,21 @@
 if(empty($_POST['submit']))
 {
 	echo "Form is not submitted!";
+	exit;
+}
+
+if(empty($_POST['fullname']))
+{
+	empty($_POST['email'])
+	{
+		echo "please fill the form";
 		exit;
-		}
-		if(empty($_POST['fullname']))
-		{
-			empty($_POST['email'])
-				{
-						echo "please fill the form";
-								exit;
-									}
-									}
+	}
+}
+$name =	$_POST['fullname'];							$email=	$_POST['email'];
+mail('ege2@njit.edu','New form submission','New	form submission:Name:$name,
+Email:$email');
 
-									$name =
-									$_POST['fullname'];
-									$email
-									=
-									$_POST['email'];
-
-									mail('ege2@njit.edu',
-									'New
-									form
-									submission',
-									'New
-									form
-									submission:
-									Name:
-									$name,
-									Email:
-									$email');
-
-									header('Location:
-									thank-you.html');
-
-									?>
+header('Location:thank-you.html');
+?>
 
