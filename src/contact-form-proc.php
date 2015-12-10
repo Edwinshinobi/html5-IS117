5aede7522152e37b5351eea04fb42378b7be64de
Edwin G Encarnacion unknown student <ege2@afsconnect1.njit.edu>
2015-12-09 19:23:08 -0500

    php example form

---
+++ src/contact-form-proc.php
@@ -0,0 +1,39 @@
+<?php
+
+if(empty($_POST['submit']))
+{
+	echo "Form is not submitted!";
+		exit;
+		}
+		if(empty($_POST['fullname']))
+		{
+			empty($_POST['email'])
+				{
+						echo "please fill the form";
+								exit;
+									}
+									}
+
+									$name =
+									$_POST['fullname'];
+									$email
+									=
+									$_POST['email'];
+
+									mail('ege2@njit.edu',
+									'New
+									form
+									submission',
+									'New
+									form
+									submission:
+									Name:
+									$name,
+									Email:
+									$email');
+
+									header('Location:
+									thank-you.html');
+
+									?>
+
